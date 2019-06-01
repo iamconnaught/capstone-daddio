e967da4ea8398f5c8d58919e40f2996b848da81c
iamconnaught <wdonnelly22@gmail.com>
2019-06-01 09:03:00 -0500

    set up name model

---
+++ models/name.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const NameSchema = new mongoose.Schema({
+	name: String,
+	ownerId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}
+});
+
+module.exports = mongoose.model('Name' NameSchema);
