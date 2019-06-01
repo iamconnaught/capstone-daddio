411c9134db6165fdcf5b32f00263672dbc7a956c
iamconnaught <wdonnelly22@gmail.com>
2019-05-31 21:04:58 -0500

    set up server.js

--- server.js
+++ server.js
@@ -5,10 +5,33 @@ const app            = express();
 const bodyParser     = require('body-parser');
 const cors           = require('cors');
 const session        = require('express-session');
-const bcrypt 		 = require('bcryptjs')
-const methodOverride = require('method-override')
-const superagent 	 = require('superagent')
+const bcrypt 		 = require('bcryptjs');
+const methodOverride = require('method-override');
+const superagent 	 = require('superagent');
 
 
 require('./db/db');
 
+//MiddleWare
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+const corsOptions = {
+	origin: process.env.FRONTEND_URL,
+	credentials: true,
+	optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const authController = require('./controllers/authController');
+app.use('/auth', authController);
+
+app.listen(process.env.PORT, () => {
+	console.log('listening on port', process.env.PORT);
+})
