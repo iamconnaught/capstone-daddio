5d2b4e89311939635fdeef7019aaf1661a9192f4
iamconnaught <wdonnelly22@gmail.com>
2019-06-01 08:53:22 -0500

    set up baby model

---
+++ models/baby.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const BabySchema = new mongoose.Schema({
+	dateOfBirth: Date,
+	name: String,
+	gender: String,
+	ownerId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}
+});
+
+module.exports = mongoose.model('Baby' BabySchema);
