107232f8f6828748ca0229631223b5ae8b28bd84
iamconnaught <wdonnelly22@gmail.com>
2019-06-01 08:58:35 -0500

    set up task model

---
+++ models/task.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+	title: String,
+	details: String,
+	isCompleted: Boolean,
+	ownerId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}
+});
+
+module.exports = mongoose.model('Task' TaskSchema);
