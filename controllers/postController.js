9ba49c72fc8e184d3fc4c1db34da3534c2c73afb
iamconnaught <wdonnelly22@gmail.com>
2019-06-02 20:38:24 -0500

    set up postController get for indivual post, update and delete

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,6 +10,7 @@ router.post('/new', async (req, res, next) => {
 			title: req.body.title,
 			text: req.body.text,
 			date: req.body.date,
+			keywords: req.body.keywords,
 			ownerId: currentUser._id
 		})
 
@@ -35,6 +36,40 @@ router.get('/', async (req, res, next) => {
 		});
 	}
 });
+
+router.get('/:id', async (req, res, next) => {
+	try {
+		if (!req.session.loggedIn) {
+			res.json({
+				data: "not logged in"
+			})
+		} else {
+			const foundPost = await Post.findById(req.params.id);
+			res.status(200).json(foundPost);
+		}
+	} catch (err){
+		next(err);
+	}
+});
+
+router.put('/:id', (req,res) => {
+	if (req.session.loggedIn) {
+		Post.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedPost) => {
+			res.status(200).json(updatedPost);
+		})
+	}
+});
+
+router.delete('/:id', async (req, res, next) => {
+	if (req.session.loggedIn) {
+		try {
+			const deletedPost = await Post.findByIdAndRemove(req.params.id);
+			res.status(200).json('deleted: ' + deletedPost);
+		} catch (err){
+			next(err)
+		}
+	}
+})
 
 
 module.exports = router;
