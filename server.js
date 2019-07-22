82ef122cafa422cf1ecf209ea058b9a6a279a7c8
iamconnaught <wdonnelly22@gmail.com>
2019-07-22 12:40:17 -0500

    cors back

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ app.use(session({
 }));
 
 
-// const corsOptions = {
-// 	origin: process.env.FRONTEND_URL,
-// 	credentials: true,
-// 	optionsSuccessStatus: 200
-// };
-// app.use(cors(corsOptions));
-app.use(cors(*));
+const corsOptions = {
+	origin: process.env.FRONTEND_URL,
+	credentials: true,
+	optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 
 const authController = require('./controllers/authController');
 app.use('/auth', authController);
