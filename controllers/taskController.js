1be6562bf0bd8c8881c1c5fbbf66a8d8c4b64156
iamconnaught <wdonnelly22@gmail.com>
2019-07-24 14:01:26 -0500

    fixed capitalization typo

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const Task = require('../models/Task');
+const Task = require('../models/task');
 
 router.post('/new', async (req, res, next) => {
 	if(req.session.loggedIn){
